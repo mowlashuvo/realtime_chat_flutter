4f58cfb06d40ffbf54592ac0447b6962def715e4
Apk Builder <arfatulmowlashuvo@gmail.com>
2022-06-06 18:47:42 +0600

    dsf

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,7 @@
+class AppConstants {
+  static const appTitle = "Euro Food Live Support";
+  static const loginTitle = "Login";
+  static const homeTitle = "Home";
+  static const settingsTitle = "Settings";
+  static const fullPhotoTitle = "Full Photo";
+}
---
+++ lib/models/message_chat.dart
@@ -0,0 +1,59 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../constants/constants.dart';
+
+class MessageChat {
+  String idFrom;
+  String idTo;
+  String timestamp;
+  String content;
+  int type;
+
+  MessageChat({
+    required this.idFrom,
+    required this.idTo,
+    required this.timestamp,
+    required this.content,
+    required this.type,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      FirestoreConstants.idFrom: this.idFrom,
+      FirestoreConstants.idTo: this.idTo,
+      FirestoreConstants.timestamp: this.timestamp,
+      FirestoreConstants.content: this.content,
+      FirestoreConstants.type: this.type,
+    };
+  }
+
+  factory MessageChat.fromDocument(DocumentSnapshot doc) {
+    String idFrom = doc.get(FirestoreConstants.idFrom);
+    String idTo = doc.get(FirestoreConstants.idTo);
+    String timestamp = doc.get(FirestoreConstants.timestamp);
+    String content = doc.get(FirestoreConstants.content);
+    int type = doc.get(FirestoreConstants.type);
+    return MessageChat(idFrom: idFrom, idTo: idTo, timestamp: timestamp, content: content, type: type);
+  }
+
+  // factory MessageChat.fromJson(String str) => MessageChat.fromMap(json.decode(str));
+  //
+  // String toJson() => json.encode(toMap());
+
+  factory MessageChat.fromMap(Map<String, dynamic> json) => MessageChat(
+    idFrom: json["idFrom"],
+    idTo: json["idTo"],
+    timestamp: json["timestamp"],
+    content: json["content"],
+    type: json["type"],
+  );
+
+  Map<String, dynamic> toMap() => {
+    "idFrom": idFrom,
+    "idTo": idTo,
+    "timestamp": timestamp,
+    "content": content,
+    "type": type,
+  };
+
+
+}
